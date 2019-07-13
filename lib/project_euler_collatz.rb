55418250d272fb4094ac3c7bf6da674494ca73d3
github username <github email address>
2019-07-13 11:43:25 -0400

    Done.

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,42 @@
+require 'pry'
+
 def even_next(n)
+  n/2
 end
 
 def odd_next(n)
+  (3 * n) + 1
 end
 
 def next_value(n)
+  n.even? ? even_next(n) : odd_next(n)
 end
 
 def collatz(n)
+  collatz_arr = []
+  while n > 1
+    collatz_arr << n
+    n = next_value(n)
+  end
+  collatz_arr << n
+  collatz_arr
 end
 
 def longest_collatz
+  n = 1000000
+  longest = 0
+  longest_length = 0
+  processed_nums = {}
+  while n > 1
+    unless !!processed_nums[n]
+      collatz_arr = collatz(n)
+      collatz_arr.each{ |v| processed_nums[v] = v }
+      if collatz_arr.length > longest_length
+        longest = n
+        longest_length = collatz_arr.length
+      end
+    end
+    n -= 1
+  end
+  longest
 end
